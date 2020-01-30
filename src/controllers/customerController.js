40b10f3f24d17e348eb4b8647d0ca1399adee60b
Elisa <egespinozap_est@utmachala.edu.ec>
2020-01-30 09:43:47 -0500

    Inicio

---
+++ src/controllers/customerController.js
@@ -0,0 +1,61 @@
+const controller = {};
+
+controller.list = (req, res) => {
+  req.getConnection((err, conn) => {
+    conn.query('SELECT * FROM clientes', (err, customers) => {
+     if (err) {
+      res.json(err);
+     }
+
+
+     console.log(customers)
+     res.render('customers', {
+        data: customers
+     });
+    });
+  });
+};
+
+controller.save = (req, res) => {
+  const data = req.body;
+  console.log(req.body)
+  req.getConnection((err, connection) => {
+    const query = connection.query('INSERT INTO clientes set ?', data, (err, customer) => {
+      console.log(customer)
+      res.redirect('/');
+    })
+  })
+};
+
+controller.edit = (req, res) => {
+  const { id } = req.params;
+  req.getConnection((err, conn) => {
+    conn.query("SELECT * FROM clientes WHERE id = ?", [id], (err, rows) => {
+      res.render('customers_edit', {
+        data: rows[0]
+      })
+    });
+  });
+};
+
+controller.update = (req, res) => {
+  const { id } = req.params;
+  const newCustomer = req.body;
+  req.getConnection((err, conn) => {
+
+  conn.query('UPDATE clientes set ? where id = ?', [newCustomer, id], (err, rows) => {
+    res.redirect('/');
+  });
+  });
+};
+
+controller.delete = (req, res) => {
+  const { id } = req.params;
+  req.getConnection((err, connection) => {
+    connection.query('DELETE FROM clientes WHERE id = ?', [id], (err, rows) => {
+      res.redirect('/');
+    });
+  });
+}
+
+module.exports = controller;
